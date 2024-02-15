a5018a127aa3dd6ce08dd3c675dfab0e802c11c6
Divyanshi Rastogi <divyanshi.rastogi5@gmail.com>
2024-02-16 01:10:22 +0530

    aboutsection

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import HeroSection from "./compoents/HeroSection";
 import Navbar from "./compoents/Navbar";
+import AboutSection from "./compoents/AboutSection";
+
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
