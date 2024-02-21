52f4ccc5365d81c886e371c9aeb3701239d9010f
Divyanshi Rastogi <divyanshi.rastogi5@gmail.com>
2024-02-21 19:37:36 +0530

    Projection-section

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import HeroSection from "./compoents/HeroSection";
-import Navbar from "./compoents/Navbar";
-import AboutSection from "./compoents/AboutSection";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectSection from "./components/ProjectSection";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectSection />
       </div>
     </main>
   );
